2381bfc5e11d10aacecdfc3dd2639b62395f31c5
Titusomega <93758145+Titusomega@users.noreply.github.com>
2023-11-14 16:12:00 +0530

    Create ma.py

---
+++ ma.py
@@ -0,0 +1,33 @@
+import random
+import array
+
+MAX_LEN = 12
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z']
+
+UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z']
+
+SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+           '*', '(', ')', '<']
+a=int(input("Enter the length of the password: "))
+s=''
+n=0
+if(a>=4):
+    while n<=a-1:
+        n=n+4
+        rand_digit = random.choice(DIGITS)
+        rand_upper = random.choice(UPCASE_CHARACTERS)
+        rand_lower = random.choice(LOCASE_CHARACTERS)
+        rand_symbol = random.choice(SYMBOLS)
+        cs=rand_symbol+rand_lower+rand_upper+rand_digit
+        s=s+cs
+    print(s[0:a])
+else:
+    print("length should be greater than 4")
